f427b67fd9e9eeb801d73a398a52258cafc52233
Thiask Alves <thiask@gmail.com>
2017-04-28 17:10:22 -0300

    Add files via upload

---
+++ siteadcnFinal/contador.php
@@ -0,0 +1,57 @@
+
+
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="contador/assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="contador/assets/css/soon.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- START BODY -->
+
+    <!-- START HEADER -->
+        <div class="container">
+            <!-- START TIMER -->
+            <div id="timer" data-animated="FadeIn">
+                <div id="days" class="timer_box"></div>
+                <div id="hours" class="timer_box"></div>
+                <div id="minutes" class="timer_box"></div>
+                <div id="seconds" class="timer_box"></div>
+            </div>
+        </div>
+        <!-- LAYER OVER THE SLIDER TO MAKE THE WHITE TEXTE READABLE -->
+       
+        <!-- END LAYER -->
+        <!-- START SLIDER -->
+        <!-- END SLIDER -->
+
+    <!-- END HEADER -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="contador/assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="contador/assets/js/modernizr.custom.js"></script>
+    <script src="contador/assets/js/bootstrap.js"></script>
+    <script src="contador/assets/js/soon/plugins.js"></script>
+    <script src="contador/assets/js/soon/jquery.themepunch.revolution.min.js"></script>
+    <script src="contador/assets/js/soon/custom.js"></script>
+  </body>
+  <!-- END BODY -->
